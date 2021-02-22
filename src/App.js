6c959777546241bddce75e73d77457bc74038a2e
missannikab <63022891+missannikab@users.noreply.github.com>
2021-02-22 16:41:41 -0600

    HostSettings and Explore Page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import RoundBox from './components/RoundBox.js'
+import RoundBox from './components/RoundBox.js';
 
 function App() {
   return (
     <div className="App">
-      <RoundBox color="teal"> 
-      <p>Example teal box</p>
+      <h1>explore</h1>
+      <RoundBox color="grey">
+      <RoundBox color="teal">
+        <p><b><u>JungKook Joggers</u></b><br></br>
+        56 members<br></br>
+        Founded <b>11/26/2003</b><br></br>
+        Running ~ 5:43/mile</p>
       </RoundBox>
-      <RoundBox color="grey"> 
-      <p>Example grey box</p>
+      <RoundBox color="orange">
+        <p><u><b>Swift Skiers</b></u><br></br>
+        13 members<br></br>
+        Founded <b>12/13/1989</b><br></br>
+        Skiing ~ 4:36/mile</p>
       </RoundBox>
-      <RoundBox color="orange"> 
-      <p>Example orange box</p>
+      <RoundBox color="darkTeal">
+        <p><u><b>Hunger For Divergent Hill Dominators</b></u><br></br>
+        56 members <br></br>
+        Founded <b>11/20/2003</b><br></br>
+        Running ~ 11:00/mile</p>
+      </RoundBox>
       </RoundBox>
     </div>
   );
 }
+
+      
+
 
 export default App;
