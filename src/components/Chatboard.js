a389740b106811bf81aeaaf7808dff5721fab71e
Henry Scheible <henry.scheible@gmail.com>
2021-03-05 12:09:37 -0600

    fix chatboard

--- src/components/Chatboard.js
+++ src/components/Chatboard.js
@@ -6,46 +6,47 @@ import './RoundBox.css';
 class Chatboard extends Component {
     constructor(props) {
         super(props);
-        this.state = {messages: [
-            {
-                outerType: "SendChat",
-                innerType: "sendChat",
-                text: "Sending a message out."
-            },
-            {
-                outerType: "ReceiveChat",
-                innerType: "receiveChat",
-                text: "And I've received your text! lol Let's make this really long and see what happens to it, shall we?"
-            }
-        ]}
+        this.state = {
+            messages: [
+                {
+                    outerType: "SendChat",
+                    innerType: "sendChat",
+                    text: "Sending a message out."
+                },
+                {
+                    outerType: "ReceiveChat",
+                    innerType: "receiveChat",
+                    text: "And I've received your text! lol Let's make this really long and see what happens to it, shall we?"
+                }
+            ]
+        }
     }
 
     render() {
-        return(
-            <div className="Chatboard">
-                <div className="BackgroundBox">
+        return (
+            <div className="BackgroundBox">
                 <RoundBox color="grey">
-                {this.state.messages.map(
-                (value) => {
-                    return (
-                        <div className={value.outerType}>
-                            <RoundBox color={value.innerType}>
-                                <p>{value.text}</p>
-                            </RoundBox>
-                        </div>
-                    );
-                }
-            )}
-            <div className="SendingMessages">
-                <input className="Typebox" type="text"
-                name="sent messages"
-                id="sent messages"
-                defaultValue="Send a message to the group"></input>
+                    <div className="viewMessages">
+                    {this.state.messages.map(
+                        (value) => {
+                            return (
+                                <div className={value.outerType}>
+                                    <RoundBox color={value.innerType}>
+                                        <p>{value.text}</p>
+                                    </RoundBox>
+                                </div>
+                            );
+                        }
+                    )}
+                    </div>
+                    <div className="SendingMessages">
+                        <input className="Typebox" type="text"
+                            name="sent messages"
+                            id="sent messages"
+                            defaultValue="Send a message to the group"></input>
+                    </div>
+                </RoundBox>
             </div>
-            </RoundBox>
-            </div>
-            </div>
-            
         );
     }
 }
