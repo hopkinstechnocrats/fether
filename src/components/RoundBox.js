ac6c9d005252d3a1af77d0b3adba98268cbe6309
Henry Scheible <henry.scheible@gmail.com>
2021-03-05 13:28:26 -0600

    chatboard message feature works

--- src/components/RoundBox.js
+++ src/components/RoundBox.js
@@ -4,7 +4,6 @@ import './RoundBox.css';
 class RoundBox extends React.Component {
 
     render() {
-        console.log(this.props.children);
         let classname = "RoundBox "+this.props.color;
         return (
             <div className={classname} >
