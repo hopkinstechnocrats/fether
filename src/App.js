1bfd8501f3a22ec8afcef6aad846f384e8768d6f
Henry Scheible <henry.scheible@gmail.com>
2021-03-05 10:58:06 -0600

    fix chatboard header name

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         <HostSettings></HostSettings>
       </div>
       <div label="Chatboard">
-        <h1>JungKook Joggers</h1>
+        <h1>Swift Skiers</h1>
         <Chatboard></Chatboard>
       </div>
       <div label="Group">
